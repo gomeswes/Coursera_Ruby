81e556be9dc28846288fc2aa21728ab215f5da0a
Wesley Gomes <gomes.wesley@gmail.com>
2017-10-18 20:29:35 +0200

    helpers and controllers logic

--- module_3/my_first_app/app/controllers/greeter_controller.rb
+++ module_3/my_first_app/app/controllers/greeter_controller.rb
@@ -1,5 +1,10 @@
 class GreeterController < ApplicationController
   def hello
+  	random_names = ["Alex", "Joe", "Michael"]
+  	@name = random_names.sample
+  	@time = Time.now
+  	@times_displayed ||= 0
+  	@times_displayed += 1 #value will not be kept through out requests.
   end
   def goodbye
   end
--- module_3/my_first_app/app/helpers/greeter_helper.rb
+++ module_3/my_first_app/app/helpers/greeter_helper.rb
@@ -1,2 +1,6 @@
 module GreeterHelper
+	def formatted_time(time)
+		#time string in AM/PM
+		time.strftime("%I:%M%p")
+	end
 end
